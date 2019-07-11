b2f29126023fe3485ac43c981ce5aee25ab3e13a
vic30004 <victor_993@msn.com>
2019-07-11 10:58:32 -0700

    mobile responsive for 768px, added array to save keys as well

--- assests/JS/app.js
+++ assests/JS/app.js
@@ -1,34 +1,59 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    'use strict';
+    // records keys that are entered and saves them to an empty array. 
+    let buffer = [];
 
-// global variables
+    document.addEventListener('keydown', event => {
+        const charList = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        const key = event.key.toLowerCase();
 
-let wins= 0;
-let numberOfGuesses= 9;
-let lettersGuessed = "";
+        // we are only interested in alphanumeric keys
+        if (charList.indexOf(key) === -1) return;
 
-//Show names, randomly selecting them and converting them to -
-let shows= ["Dark","Stranger Things", "Black Mirror", "13 Reasons Why"];		
-let answerArray=[];
-let randomShow = shows[Math.floor(Math.random()*shows.length)]
+        buffer.push(key);
+        console.log(buffer);
+    });
 
-for (let i=0; i<shows.length; i++){
-	
-  answerArray[i]= '_';
 
-}
-console.log(answerArray)
-
-let remainingLetters = shows.length;
-
-while (remainingLetters >0){
-    console.log(answerArray.join(" "));
-
+let loop = (arr) => {
+    for (let i=0; i<arr.length; i++){
+        arr[i]
+    }
 }
 
-document.addEventListener('keydown', event => {
-    const key = event.key.toLowerCase();
-    console.log(key);
-});
+    // global variables
+    let wins = "0";
+    let numberOfGuesses = "9";
+    let lettersGuessed = "";
 
+    //Show names, randomly selecting them and converting them to -
+    let shows = ["Dark", "Stranger Things", "Black Mirror", "13 Reasons Why"];
+    let answerArray = [];
+    let randomShow = shows[Math.floor(Math.random() * shows.length)];
+
+    for (let i = 0; i < shows.length; i++) {
+
+        answerArray[i] = '_';
+
+    }
+    
+    let guess= document.getElemenetsByClassName("guess");
+    guess.ineerHtml = "smthn";
+
+    let remainingLetters = shows.length;
+
+    //if (remainingLetters > 0) {
+    //    console.log(answerArray.join(" "));
+
+  //  }
+
+    for (let j = 0; j<shows.length; j++){
+        if (shows[j] === loop(buffer)) {
+            answerArray[j] = loop(buffer);
+            remainingLetters--;
+        }
+    }
+
+   console.log(answerArray)
 });
