529a1e25c80fad0b8ba70bdd335c1deb11c68c20
vic30004 <victor_993@msn.com>
2019-07-11 20:02:22 -0700

    reset for when game ends and able to detect wins

--- assests/JS/app.js
+++ assests/JS/app.js
@@ -1,49 +1,104 @@
 // global variables
 let wins = 0;
-let numberOfGuesses = 9;
+let numberOfGuesses = 13;
 const charList = 'abcdefghijklmnopqrstuvwxyz0123456789';
-//Show names, randomly selecting them and converting them to _
 let shows = ["Dark", "Stranger Things", "Black Mirror", "Money Heist"];
+
+// Simple way to get object item
+const videos = {
+  "Dark": "show",
+  addUrl: function addingUrl(){
+    let hidePic = document.getElementById("main-img");
+    hidePic.style.visibility="hidden";
+    let newUrl = document.getElementById("new-video");
+    newUrl.setAttribute('src',"https://www.youtube.com/embed/BZ5OCIJVErw" )},
+  
+
+  strangerThings: {
+  "Stranger Things": "show",
+  addUrl: function addingUrl(){
+    let hidePic = document.getElementById("main-img");
+    hidePic.style.visibility="hidden";
+    let newUrl = document.getElementById("new-video");
+    newUrl.setAttribute('src',"https://www.youtube.com/watch?v=mnd7sFt5c3A" )
+},
+
+}
+}
+
+ //let newVideoUrl = videos[word];
+// updateVideo(newVideoUrl);
+
 let word;
 let buffer = [];
 let correctGuesses = [];
 
+const resetButton = document.getElementById("reset-btn");
+
+
+let testString = ""
 
 selectRandomWord();
 updateGuesses();
 updateCurrentWord();
 
+// recently added
 function reset(){
+  // add hidden class to button element
   correctGuesses = [];
   buffer = [];
-  numberOfGuesses = 9;
+  numberOfGuesses = 13;
   selectRandomWord();
   updateGuesses();
   updateCurrentWord();
+  updateGuessedLetters(key);
+  
 }
 
 function selectRandomWord(){
   //This will loop through the shows and save a show under current show as ---- 
   word = shows[Math.floor(Math.random() * shows.length)];
+  word.split("").forEach(c => testString += "_");
+  console.log(testString)
 }
+
 
 function updateCurrentWord(){
   let currentWord = document.getElementById("current-word");
   currentWord.innerHTML = ""; //this clears the content of the html element
-  for (let i = 0; i < word.length; i++) {
+  testString = '';
+
+  for (let i = 0, l = word.length; i < l; i++) {
       let currentLetter = word[i];
       let letterToUse = "_";
       if(currentLetter === " "){
-        letterToUse = "  ";
+        letterToUse = " ";
       } else if(correctGuesses.indexOf(word[i].toLowerCase()) !== -1){
         letterToUse = word[i];
       }
       let currWord = document.createElement("span");
       currWord.classList.add("current-word-letter");
       currWord.textContent = letterToUse;
+      testString += letterToUse;
       currentWord.appendChild(currWord)
-  }
+    }
+
+    // Perform endgame check
+    console.log(testString);
+    console.log(word === testString)
+
+    const gameIsOver = !testString.includes("_");   
+    if ( gameIsOver ) {
+      
+      reset();
+     return wins++;
+      }
+      
+      console.log('GAME OVER')
+    
+  
 }
+
 
 function updateGuesses(){
   // adding number of guesses left to document
@@ -84,7 +139,8 @@ function updateGuessedLetters(key){
     numberOfGuesses--;
     updateGuesses();
     if(numberOfGuesses === 0){
-      //you lost
+      reset()
+      
     }
   }
 }
@@ -94,6 +150,7 @@ function updateGuessedLetters(key){
 document.addEventListener('DOMContentLoaded', () => {
 
     let guess = document.getElementById("guess");
+
 
     document.addEventListener('keydown', event => {
         
